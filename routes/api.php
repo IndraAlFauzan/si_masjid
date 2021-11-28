de26c07604d13119de9acc26ab8de2476fb09fdb
IndraAlFauzan <39118928+IndraAlFauzan@users.noreply.github.com>
2021-11-28 14:04:19 +0700

    scond commit

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
